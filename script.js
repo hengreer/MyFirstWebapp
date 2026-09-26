c5353f7e6f8519f3ef938eec42436dec90ceb7d2
Henry Greer <hengreer@hotmail.com>
2026-09-26 16:14:06 +0100

    Added week 5 API. Large amount of work required. Have a new random receipe link.

--- script.js
+++ script.js
@@ -70,4 +70,104 @@ form.addEventListener('reset', function(){
      document.getElementById("char-length").innerText = "0";
 });
 
+        async function getMeal() {
+
+            const sections = document.getElementsByClassName("meal");
+            const contents = document.getElementsByClassName("mealId");
+
+            // Clear previous meal
+            for (let content of contents) {
+                content.innerHTML = "";
+            }
+
+            // Show meal sections
+            for (let section of sections) {
+                section.style.display = "block";
+            }
+
+            const urlAddress =
+                "https://api.freeapi.app/api/v1/public/meals/meal/random";
+
+            const response = await fetch(urlAddress);
+
+            const meal = await response.json();
+
+            const array = Object.keys(meal.data);
+
+            let j = 1;
+
+            for (let i = 0; i < array.length; i++) {
+
+                const key = array[i];
+                const value = meal.data[key];
+                //const displayKey = key.replace(/^str/, "");
+
+                if (value !== null && value !== undefined && value !== "") {
+
+                    if (key === "strMeal") {
+                        document.getElementById("dish").innerHTML += value + "<br>";
+                    }
+
+                    else if (key === "strCategory") {
+                        document.getElementById("category").innerHTML += value + "<br>";
+                    }
+
+                    else if (key === "strArea") {
+                        document.getElementById("area").innerHTML += value + "<br>";
+                    }
+
+                    else if (key.includes("Ingredient")) {
+
+                        const measureKey = key.replace("Ingredient", "Measure");
+
+                        displayAnswer(
+                            `${j} : ${value}: ${meal.data[measureKey]}`
+                        );
+
+                        j++;
+                    }
+
+                    else if (key === "strInstructions") {
+                        document.getElementById("instructions").innerHTML += value + "<br>";
+                    }
+
+                    else if (key === "strSource") {
+                        const link = document.createElement("a");
+
+                    link.className = "text-link";
+                    link.href = value;
+                    link.textContent = value;
+                    link.target = "_blank";
+                    link.rel = "noopener noreferrer";
+
+                    document.getElementById("source").appendChild(link);
+                    }
+
+                    else if (key === "strMealThumb") {
+                        const image = document.createElement("img");
+                        image.src = value;
+                        image.alt = "Meal Image";
+                        document.getElementById("thumb").appendChild(image);
+                    }
+
+                    else if (key === "strYoutube") {
+                        const link = document.createElement("a");
+
+                        link.href = value;
+                        link.textContent = "YouTube Receipe Link";
+                        link.target = "_blank";
+
+                        document.getElementById("youtube").appendChild(link);
+                    }
+
+                    console.log(key);
+                }
+            }
+
+            function displayAnswer(text) {
+                document.getElementById("ingredients").innerHTML +=
+                    "<br>" + text + "<br>";
+            }
+        }
+
 //https://www.w3schools.com/js/js_validation.asp - Add in javascript validation for the form
